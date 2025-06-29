43b7cba15a831b56a5b24637c17f5f2f9febaeb1
Keys <keys14052002@gmail.com>
2025-06-29 20:42:16 +0000

    Clean up code

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Enter all the numbers separated with comma or whitespace \n(NB: Do not separate the numbers with whitespace after a comma)");
+        System.out.println("Enter all the numbers separated with comma or whitespace");
         String numbersInput = scanner.nextLine();
         
         String[] numbersToArr = numbersInput.split("[,\\s+(, )]+"); // Splitting with multiple delimiters
@@ -21,19 +21,19 @@ public class Main {
                   nums.add(Double.parseDouble(numbersToArr[i]));
 //                    numbers[i] = Double.parseDouble(numbersToArr[i]);
             } catch(NumberFormatException e) {
-                System.out.println("Warning: Enter only integers or decimals");
-//                scanner.close();
+                System.out.println("Warning: Enter only integers or decimals\n");
                 System.exit(0);
             }
         }
         System.out.println("Enter the number you want to search");
-        Double numToBeSearched = scanner.nextDouble();
         
+        double numToBeSearched = scanner.nextDouble();
+        System.out.println("Search with sentinel: The number can be found at index " + SequentialSearch.searchWithSentinel(nums, numToBeSearched)); // Runtime in Big O Notation: O(n)
         
         /*
             The algorithm with the sentinel appended at the end of list is slightly faster than without sentinel since there exist a simpler check in the loop body, so the                    iteration goes faster.
         */
-        System.out.println("Search with sentinel: The number can be found at index " + SequentialSearch.searchWithSentinel(nums, numToBeSearched)); // Runtime in Big O Notation: O(n)
+        
 //        System.out.println("Search without sentinel: " + SequentialSearch.searchWithoutSentinel(nums, 6)); // Runtime in Big O Notation: O(n), Execution time: 13.2883ms
         scanner.close();
     }
