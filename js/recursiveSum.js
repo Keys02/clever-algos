854666048b3996f97a19035775ddf60b4c7e7000
Keys <keys14052002@gmail.com>
2025-08-25 09:05:49 +0000

    feat: add recursive implementation of sum function

---
+++ js/recursiveSum.js
@@ -0,0 +1,19 @@
+/*--------------------------------------------------------
+    Recursive Implementation of the sum function
+    
+    Explanation
+    slice(0): This takes two arguments the start
+    and end, but when only one argument is passed
+    e.g 1 or 2 it removes one or two elements
+    respectively starting from the beginning 
+    of the array
+/*-------------------------------------------------------- */
+// ES5 
+function sumArrayES5(array) {
+    return (array.length === 0) ? 0 : array[0] + sum(array.slice(1))
+}
+
+// ES6
+let sumArrayES6 = (array) => (array.length === 0) ? 0 : array[0] + sum(array.slice(1))
+
+console.log(sum([1,2,3,4,5]))
