e54c20040e26a6ce2787a073fc2fdb11ec2f0a50
Keys <keys14052002@gmail.com>
2026-04-27 10:22:51 +0000

    feat(python>>non-recursive): add non-recursive binary search

---
+++ python/non-recursive-algos/binary_search.py
@@ -0,0 +1,19 @@
+def binary_search(haystack, needle):
+    low = 0
+    high = len(haystack) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        guess = haystack[mid]
+
+        if guess > needle:
+            high = mid - 1
+        elif guess < needle:
+            low = mid + 1
+        else:
+            return mid
+    return None;
+
+print(binary_search([1, 3, 8, 9, 20], 20))
+
