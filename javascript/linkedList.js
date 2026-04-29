956f1535315e55b258dce46e1ba649eb0a850811
Keys <keys14052002@gmail.com>
2026-04-29 10:24:27 +0000

    explore(js): implement a linked list

---
+++ javascript/linkedList.js
@@ -0,0 +1,23 @@
+class Node {
+    constructor(data, next = null) {
+        this.data = data
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.size = 0
+    }
+
+    insertFirst(data) {
+        this.head = new Node(data, this.head) // use the previous head
+    }
+}
+
+let ll = new LinkedList()
+ll.insertFirst(100)
+ll.insertFirst(200)
+ll.insertFirst(300)
+console.log(ll)
