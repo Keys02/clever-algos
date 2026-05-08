5ddceebdb81e014124029de1e04b0fa943a7dec3
Keys <keys14052002@gmail.com>
2026-05-08 08:52:42 +0000

    fix the bug when printing a sequence with less than three elements

--- javascript/recursive-algos/fibonacci.js
+++ javascript/recursive-algos/fibonacci.js
@@ -7,11 +7,14 @@ function fib(n) {
 }
 
 function fibList(n) {
+    if (n == 0) return [];
+    else if (n == 1) return [0]
+    else if (n == 2) return [0, 1]
+    
     let fibSeq = [0, 1]
-
-    while(n != fibSeq.length) fibSeq.push(fibSeq[fibSeq.length - 1] + fibSeq[fibSeq.length - 2])
+    while (n != fibSeq.length) fibSeq.push(fibSeq[fibSeq.length - 1] + fibSeq[fibSeq.length - 2])
 
     return fibSeq
 }
 
-console.log(fibList(10))
+console.log(fibList(5))
